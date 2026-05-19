3e0eaa35e658d8ae8427198521a5aae8ec1f6959
Mariana Guazzelli <guazzinha@gmail.com>
2026-05-18 21:37:23 -0300

    Adicionando classes Transacao e Categoria

--- src/main/java/com/financeiro/model/Categoria.java
+++ src/main/java/com/financeiro/model/Categoria.java
@@ -1,4 +1,34 @@
 package com.financeiro.model;
 
 public class Categoria {
+
+    private int id;
+    private String nome, tipo;
+
+    public Categoria() {
+    }
+
+    public Categoria(int id, String nome, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTipo() {
+        return tipo;
+    }
 }
--- src/main/java/com/financeiro/model/Transacao.java
+++ src/main/java/com/financeiro/model/Transacao.java
@@ -1,4 +1,63 @@
 package com.financeiro.model;
 
 public class Transacao {
+    private int id;
+    private String tipo;
+    private double valor;
+    private String data;
+    private String descricao;
+    private int categoriaId;
+
+    public Transacao() {}
+
+    public Transacao(int id, String tipo, double valor, String data, String descricao, int categoriaId) {
+        this.id = id;
+        this.tipo = tipo;
+        this.valor = valor;
+        this.data = data;
+        this.descricao = descricao;
+        this.categoriaId = categoriaId;
+    }
+
+    public int getId() {
+        return id; }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCategoriaId() {
+        return categoriaId;
+    }
+    public void setCategoriaId(int categoriaId) {
+        this.categoriaId = categoriaId;
+    }
 }
